8e2f1963758d9e734663e7cf20a1d83793d62d6f
slkavinda <145237216+slkavinda@users.noreply.github.com>
2024-04-29 10:11:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~1f8d3I9tc040ff41a1f8528254c562e1dbb4', //your ssid to run bot
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
@@ -45,6 +45,6 @@ module.exports = {
     OPEN_AI: process.env.OPEN_AI,
     ELEVENLABS: process.env.ELEVENLABS,
     INRL_KEY: process.env.INRL_KEY || 'free50_inrl',
-    OCR_KEY: (process.env.OCR_KEY || 'K84003107488957').trim(),
+    OCR_KEY: (process.env.OCR_KEY || '94778320156').trim(),
     DATABASE: DB_URL ? new Sequelize(DB_URL,{dialect:'postgres',ssl:true,protocol: 'postgres', dialectOptions: {native: true,ssl:{require: true,rejectUnauthorized: false}}, logging: false}) : new Sequelize({dialect:'sqlite',storage:'./database.db',logging:false}) 
 };
